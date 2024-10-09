03a327f7a768f47121fe3c9127537f03cb4c9b44
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-10-10 03:53:01 +0700

    add bodyParser

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const listFood = require("./model/listFood");
 
 const app = express();
 
 const PORT = 3200;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
     const foods = await listFood.findAll();
